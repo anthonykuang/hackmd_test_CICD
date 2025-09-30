5478eb6f5c30cc17075eaac7cb04fcfe42b742d9
Anthony <orangecat38@yahoo.com.tw>
2025-09-30 16:15:06 +0800

    set pages

--- python_project1/tests/test_project1.py
+++ python_project1/tests/test_project1.py
@@ -4,7 +4,6 @@ def test_add():
     assert calc.add(2, 3) == 5
     assert calc.add(-1, 1) == 0
     assert calc.add(0, 0) == 0
-    assert calc.add(0, 0) == 0
 
 def test_subtract():
     calc = Calculator()
